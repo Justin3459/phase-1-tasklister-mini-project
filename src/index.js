9dea7f8c508fbaab88047c9ec125b97cf8bb3338
Justin <justin3459@hotmail.com>
2023-04-20 12:19:05 -0400

    added delete button and function

--- src/index.js
+++ src/index.js
@@ -9,8 +9,24 @@ function addEventListeners() {
 
 function handleFormSubmit(e) {
   e.preventDefault()
-  console.log(e)
   const task = e.target[0].value
-  console.log(task)
-  //const task = document.getElementById("new-task-description")
+  //const task = e.target["new-task-description"].value
+  displayTask(task)
+}
+
+function displayTask (task){
+
+  const taskUl = document.getElementById("tasks")
+  const taskLi = document.createElement('li')
+  const removeTask = document.createElement('button')
+
+  removeTask.textContent = "X"
+  removeTask.addEventListener("click", deletefnc )
+  taskLi.textContent = task + " "
+  taskLi.appendChild(removeTask)
+  taskUl.appendChild(taskLi)
+}
+
+function deletefnc (e){
+  e.target.parentNode.remove()
 }
