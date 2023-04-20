2d47d4d1b32a899a1321edb641d21d28087350b3
Justin <justin3459@hotmail.com>
2023-04-20 12:06:40 -0400

    displays tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  addEventListeners()
 });
+function addEventListeners() {
+  const form = document.getElementById("create-task-form")
+  form.addEventListener('submit', handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  console.log(e)
+  const task = e.target[0].value
+  console.log(task)
+  //const task = document.getElementById("new-task-description")
+}
